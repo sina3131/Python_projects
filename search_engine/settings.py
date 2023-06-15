f8e175a9f5cc645cb1cc938a14d2418257aaf6d4
sina <sina@sinas-MacBook-Pro.local>
2023-06-15 22:00:48 +0200

    sdf

--- search_engine/settings.py
+++ search_engine/settings.py
@@ -1,5 +1,5 @@
-SEARCH_KEY = "AIzaSyDQn6JZeJIL_WJdUfpCnCntK3eQgRlJZCc"
-SEARCH_ID = "844f37d6d23414f54"
+SEARCH_KEY = "YOUR API KEY HERE"
+SEARCH_ID = "YOUR SEARCH ID or CX"
 COUNTRY = "sv"
 
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
